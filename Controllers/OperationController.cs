2e6847a16f7dc677ef727dad93301ddf36787102
Daavvvvvv <davidvelez006@gmail.com>
2024-04-07 22:22:15 -0500

    Adding HTTP methods

--- Controllers/OperationController.cs
+++ Controllers/OperationController.cs
@@ -8,6 +8,16 @@ namespace Backend.Controllers
     public class OperationController : ControllerBase
     {
         [HttpGet]
-        public decimal Add(decimal a, decimal b) => a + b;
+        public decimal Get(decimal a, decimal b) => a + b;
+
+        [HttpPost]
+        public decimal Add(decimal a, decimal b) => a - b;
+
+        [HttpPut]
+        public decimal Edit(decimal a, decimal b) => a * b;
+
+        [HttpDelete]
+        public decimal Delete(decimal a, decimal b) => a / b;
+
     }
 }
