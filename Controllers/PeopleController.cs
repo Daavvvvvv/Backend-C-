4a1d56af75bb272f80c2e4a7bc57172e1eb3c3d1
Daavvvvvv <davidvelez006@gmail.com>
2024-04-08 22:08:13 -0500

    Added a new controller to test how to send http gets of people with '/all'

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("all")]
+        public List<People> GetPeople() => Repository.People;
+    }
+
+    public class Repository
+    {
+        public static List<People> People = new List<People>
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Foo",
+                Birthdate = new DateTime(2003, 05, 20),
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Carlos",
+                Birthdate = new DateTime(2001, 05, 20),
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Fernando",
+                Birthdate = new DateTime(2000, 05, 20),
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Isabel",
+                Birthdate = new DateTime(1993, 05, 20),
+            },
+        };
+    }
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+
+    }
+}
--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f89472521d3f72b108194357a10063abb0c41942")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8baeff295fba503cba952f76e06f5529c3ed30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
